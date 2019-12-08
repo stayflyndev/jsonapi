304e8e582120c6e56ae3e72a8f238121105ef8f2
yourtechsis <aretee64@gmail.com>
2019-12-08 11:30:56 -0700

    Updates

---
+++ precache-manifest.daa27d8b27ca1a75de7316c36f65d64c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "166736bbb5473a5296346f3efef3d819",
+    "url": "/jsonapi/index.html"
+  },
+  {
+    "revision": "86b8b059b8830a5d9f0e",
+    "url": "/jsonapi/static/css/main.657a687b.chunk.css"
+  },
+  {
+    "revision": "ea8c4d3182b841ba68be",
+    "url": "/jsonapi/static/js/2.75f3ae91.chunk.js"
+  },
+  {
+    "revision": "86b8b059b8830a5d9f0e",
+    "url": "/jsonapi/static/js/main.0271ed41.chunk.js"
+  },
+  {
+    "revision": "f43b4b24865c6bf10847",
+    "url": "/jsonapi/static/js/runtime-main.0e8665f5.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/jsonapi/static/media/logo.25bf045c.svg"
+  }
+]);
